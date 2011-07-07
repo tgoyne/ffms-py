5007021080ab2033fe7d11206a4354cda816ad63
Thomas Goyne <plorkyeran@aegisub.org>
2011-07-07 16:45:52 -0700

    Export a few functions

--- src/ffms-py.cpp
+++ src/ffms-py.cpp
@@ -2,14 +2,36 @@
 
 #define BOOST_PYTHON_STATIC_LIB
 
-#include <boost/python/module.hpp>
-#include <boost/python/def.hpp>
+#include <boost/python.hpp>
 
-void init() {
-	FFMS_Init(0, 0);
+static void init_ffms() {
+	static bool initialized = false;
+	if (!initialized) {
+		FFMS_Init(0, 1);
+		initialized = true;
+	}
 }
+
+static int version() {
+	init_ffms();
+	return FFMS_GetVersion();
+}
+
+static int get_log_level() {
+	init_ffms();
+	return FFMS_GetLogLevel();
+}
+
+static void set_log_level(int level) {
+	init_ffms();
+	FFMS_SetLogLevel(level);
+}
+
+#define EXPORT(a) def(#a, a)
 
 BOOST_PYTHON_MODULE(ffms2) {
 	using namespace boost::python;
-	def("init", init);
+	EXPORT(version);
+	EXPORT(get_log_level);
+	EXPORT(set_log_level);
 }
