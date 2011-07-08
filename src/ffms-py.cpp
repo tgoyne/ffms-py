a81b9e1ca04dccf7437488b1360bd39bd8c4c36e
Thomas Goyne <plorkyeran@aegisub.org>
2011-07-08 14:59:20 -0700

    Export the C API structs

--- src/ffms-py.cpp
+++ src/ffms-py.cpp
@@ -7,9 +7,71 @@
 
 #include "enum_mappings.h"
 
+
+
 BOOST_PYTHON_MODULE(ffms2) {
 	FFMS_Init(0, true);
+
 	using namespace boost::python;
+
+	class_<FFMS_ErrorInfo>("error_info")
+		.def_readonly("error_type", &FFMS_ErrorInfo::ErrorType)
+		.def_readonly("sub_type", &FFMS_ErrorInfo::SubType)
+		.def_readonly("buffer_size", &FFMS_ErrorInfo::BufferSize)
+		.def_readonly("buffer", &FFMS_ErrorInfo::Buffer);
+
+	class_<FFMS_Frame>("frame")
+		.def_readonly("data", &FFMS_Frame::Data)
+		.def_readonly("linesize", &FFMS_Frame::Linesize)
+		.def_readonly("encoded_width", &FFMS_Frame::EncodedWidth)
+		.def_readonly("encoded_height", &FFMS_Frame::EncodedHeight)
+		.def_readonly("encoded_pixel_format", &FFMS_Frame::EncodedPixelFormat)
+		.def_readonly("scaled_width", &FFMS_Frame::ScaledWidth)
+		.def_readonly("scaled_height", &FFMS_Frame::ScaledHeight)
+		.def_readonly("converted_pixel_format", &FFMS_Frame::ConvertedPixelFormat)
+		.def_readonly("key_frame", &FFMS_Frame::KeyFrame)
+		.def_readonly("repeat_pict", &FFMS_Frame::RepeatPict)
+		.def_readonly("interlaced_frame", &FFMS_Frame::InterlacedFrame)
+		.def_readonly("top_field_first", &FFMS_Frame::TopFieldFirst)
+		.def_readonly("pict_type", &FFMS_Frame::PictType);
+
+	class_<FFMS_TrackTimeBase>("track_time_base")
+		.def_readonly("num", &FFMS_TrackTimeBase::Num)
+		.def_readonly("den", &FFMS_TrackTimeBase::Den);
+
+	class_<FFMS_FrameInfo>("frame_info")
+		.def_readonly("pts", &FFMS_FrameInfo::PTS)
+		.def_readonly("repeat_pict", &FFMS_FrameInfo::RepeatPict)
+		.def_readonly("key_frame", &FFMS_FrameInfo::KeyFrame);
+
+	class_<FFMS_VideoProperties>("video_properties")
+		.def_readonly("fps_denominator", &FFMS_VideoProperties::FPSDenominator)
+		.def_readonly("fps_numerator", &FFMS_VideoProperties::FPSNumerator)
+		.def_readonly("rff_denominator", &FFMS_VideoProperties::RFFDenominator)
+		.def_readonly("rff_numerator", &FFMS_VideoProperties::RFFNumerator)
+		.def_readonly("num_frames", &FFMS_VideoProperties::NumFrames)
+		.def_readonly("sar_num", &FFMS_VideoProperties::SARNum)
+		.def_readonly("sar_den", &FFMS_VideoProperties::SARDen)
+		.def_readonly("crop_top", &FFMS_VideoProperties::CropTop)
+		.def_readonly("crop_bottom", &FFMS_VideoProperties::CropBottom)
+		.def_readonly("crop_left", &FFMS_VideoProperties::CropLeft)
+		.def_readonly("crop_right", &FFMS_VideoProperties::CropRight)
+		.def_readonly("top_field_first", &FFMS_VideoProperties::TopFieldFirst)
+		.def_readonly("color_space", &FFMS_VideoProperties::ColorSpace)
+		.def_readonly("color_range", &FFMS_VideoProperties::ColorRange)
+		.def_readonly("first_time", &FFMS_VideoProperties::FirstTime)
+		.def_readonly("last_time", &FFMS_VideoProperties::LastTime);
+
+	class_<FFMS_AudioProperties>("audio_properties")
+		.def_readonly("sample_format", &FFMS_AudioProperties::SampleFormat)
+		.def_readonly("sample_rate", &FFMS_AudioProperties::SampleRate)
+		.def_readonly("bits_per_sample", &FFMS_AudioProperties::BitsPerSample)
+		.def_readonly("channels", &FFMS_AudioProperties::Channels)
+		.def_readonly("channel_layout", &FFMS_AudioProperties::ChannelLayout)
+		.def_readonly("num_samples", &FFMS_AudioProperties::NumSamples)
+		.def_readonly("first_time", &FFMS_AudioProperties::FirstTime)
+		.def_readonly("last_time", &FFMS_AudioProperties::LastTime);
+
 	def("get_version", FFMS_GetVersion);
 	def("get_log_level", FFMS_GetLogLevel);
 	def("set_log_level", FFMS_SetLogLevel);
